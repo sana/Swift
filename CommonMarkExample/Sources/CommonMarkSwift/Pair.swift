cb7438a72fa2bc8d9a2648e8782a392a0b7fca60
Laurentiu Dascalu <dascalu.laurentziu@gmail.com>
2019-03-02 11:14:06 -0800

    Finishing the implementation

---
+++ CommonMarkExample/Sources/CommonMarkSwift/Pair.swift
@@ -0,0 +1,23 @@
+//
+//  Pair.swift
+//  CommonMarkSwift
+//
+//  Created by Laurentiu Dascalu on 3/2/19.
+//
+
+import Foundation
+
+struct Pair<U, V> {
+    let first: U
+    let second: V
+}
+
+extension Pair : Equatable, Hashable where U : Hashable, V : Hashable {
+    static func ==(lhs: Pair, rhs: Pair) -> Bool {
+        return lhs.first == rhs.first && lhs.second == rhs.second
+    }
+
+    var hashValue: Int {
+        return first.hashValue ^ second.hashValue
+    }
+}
--- CommonMarkExample/Tests/CommonMarkSwiftTests/CommonMarkSwiftTests.swift
+++ CommonMarkExample/Tests/CommonMarkSwiftTests/CommonMarkSwiftTests.swift
@@ -1,4 +1,5 @@
 import CommonMarkSwift
+import Ccmark
 import XCTest
 
 final class CommonMarkExampleTests: XCTestCase {
@@ -11,7 +12,38 @@ final class CommonMarkExampleTests: XCTestCase {
         XCTAssertEqual(markdownHTML, "<p><em>Hello world!</em></p>\n")
     }
 
+    func testWrapperImplementation() throws {
+        let markdown = "*Hello world!*"
+        let document = try cmark_parse_document(markdown, 13, CMARK_OPT_DEFAULT).required()
+
+        let documentNode = Node(node: document)
+        let visitor = CommonMarkExampleTestsVisitor()
+        visitor.visit(documentNode, depth: 0)
+
+        let expectedPair = Pair<String, Int>(first: "*Hello world!", second: 2)
+        let expectedLiterals = Set([expectedPair])
+        XCTAssertEqual(visitor.literals, expectedLiterals)
+    }
+
     static var allTests = [
         ("testExample", testExample),
     ]
+}
+
+// MARK:- CommonMarkExampleTestsVisitor
+
+private class CommonMarkExampleTestsVisitor {
+    var literals: Set<Pair<String, Int>>
+    init() {
+        literals = Set<Pair<String, Int>>()
+
+    }
+
+    func visit(_ node: Node, depth: Int) {
+        if let literal = node.literal {
+            literals.insert(Pair<String, Int>(first: literal, second: depth))
+        }
+
+        node.children.forEach { visit($0, depth: depth + 1) }
+    }
 }
